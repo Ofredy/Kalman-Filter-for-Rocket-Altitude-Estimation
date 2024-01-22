17ed1481ed4fb2e8a8d4025c21e128638006d008
Ofredy <orellanafredy96@gmail.com>
2024-01-21 23:36:59 -0600

    created the results folder

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@ int main()
 {
 
     std::ofstream output_file;
-    output_file.open("data_rocket_altitude_estimation.txt");
+    output_file.open("results/data_rocket_altitude_estimation.txt");
 
     KalmanFilter rocket_altitude_estimation;
     rocket_altitude_estimation.rocket_altitude_kalman_filter(output_file, F, G, Q, H, R, ALTITUDE_MEASUREMENTS, ACCELERATION_MEASUREMENTS);
